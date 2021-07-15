3d53b370da42b375b3cc4d87eab3eddd061ab436
sirArthurDayne <xavierivan2000@hotmail.com>
2021-07-15 16:53:36 -0500

    conversion de word a pdf

--- Script.py
+++ Script.py
@@ -1,11 +1,13 @@
 import os
 import docx
 import sys
+import docx2pdf
 
 # ---------- Declaraciones ----------
-user = input("Ingresa el user de la PC")
-ruta_de_proyectos = "C:\\Users\\{}\\AndroidStudioProjects".format(user)
-escritorio = "C:\\Users\\{}\\Desktop".format(user)
+operation = input("1. android to word\n 2. word to pdf\n")
+ruta_de_proyectos = "D:\\Dev\\Proyects\\androidstudio"
+escritorio = "D:\\vainas de la U\\desarrolloMovil"
+wordToPdf = "D:\\vainas de la U\\desarrolloMovil\\Word to Pdf"
 try:
     os.mkdir(os.path.join(escritorio, "Android a Word"))
 except:
@@ -42,46 +44,79 @@ def es_mp4(nombre):
     nombre_extension = os.path.splitext(nombre)[-1].lower()
     return nombre_extension in extensiones
 
+
+def es_docx(nombre):
+    return nombre[-5:] == ".docx"
+
 # ---------- Ejecución ----------
 
-# Obtener lista de proyectos de Android -->
-try:
-    os.chdir(ruta_de_proyectos)
-except:
-    print("Esa ruta no existe. Verifica el nombre de usuario.")
-    sys.exit()
-proyectos = os.listdir()
+if operation == "1":
+    # Obtener lista de proyectos de Android -->
+    try:
+        os.chdir(ruta_de_proyectos)
+    except:
+        print("Esa ruta no existe. Verifica el nombre de usuario.")
+        sys.exit()
+    proyectos = os.listdir()
 
-# Cambiar a folder nuevo en escritorio -->
-try:
-    os.chdir(os.path.join(escritorio, "Android a Word"))
-except:
-    print("Esa ruta no existe. Verifica el nombre de usuario.")
-    sys.exit()
+    # Cambiar a folder nuevo en escritorio -->
+    try:
+        os.chdir(os.path.join(escritorio, "Android a Word"))
+    except:
+        print("Esa ruta no existe. Verifica el nombre de usuario.")
+        sys.exit()
 
-# Crear documentos por cada proyecto -->
-for proyecto_i in proyectos:
-    document = docx.Document()
-    
-    ruta_i = os.path.join(ruta_de_proyectos, proyecto_i)
-    archivos_1 = {}
-    for root, dirs, files in os.walk(ruta_i):
-        if filtro_por_carpeta(root, proyecto_i):
-            for name in files:
-                if filtro_por_extension(name) and filtro_por_archivo(name):
-                    archivos_1[name] = os.path.join(root, name)
+    # Crear documentos por cada proyecto -->
+    for proyecto_i in proyectos:
+        document = docx.Document()
 
-    # Copiar contenido de archivos al documento -->
-    for i in archivos_1:
-        document.add_heading(i, level=1)
-        if es_imagen(i):
-            document.add_picture(archivos_1[i], width=docx.shared.Inches(1.25))
-        elif es_mp3(i):
-            document.add_paragraph("Archivo de sonido.")
-        elif es_mp4(i):
-            document.add_paragraph("Archivo de video.")
-        else:
-            f = open(archivos_1[i], "r")
-            document.add_paragraph(f.read())
-      
-    document.save(proyecto_i + '.docx')
+        ruta_i = os.path.join(ruta_de_proyectos, proyecto_i)
+        archivos_1 = {}
+        for root, dirs, files in os.walk(ruta_i):
+            if filtro_por_carpeta(root, proyecto_i):
+                for name in files:
+                    if filtro_por_extension(name) and filtro_por_archivo(name):
+                        archivos_1[name] = os.path.join(root, name)
+
+        # Copiar contenido de archivos al documento -->
+        for i in archivos_1:
+            document.add_heading(i, level=1)
+            if es_imagen(i):
+                document.add_picture(archivos_1[i], width=docx.shared.Inches(1.25))
+            elif es_mp3(i):
+                document.add_paragraph("Archivo de sonido.")
+            elif es_mp4(i):
+                document.add_paragraph("Archivo de video.")
+            else:
+                f = open(archivos_1[i], "r")
+                document.add_paragraph(f.read())
+
+        document.save(proyecto_i + '.docx')
+elif operation == "2":
+    print("creando ruta, buscando archivos word...")
+    #ruta a los documentos word
+    try:
+        os.chdir(os.path.join(escritorio, "Android a Word"))
+    except:
+        print("Esa ruta no existe")
+        sys.exit()
+    #lista los word dentro de la ruta
+    wordList = os.listdir()
+    #crea carpeta para guardar los pdf y se mueve para alla
+    try:
+        os.mkdir(wordToPdf)
+    except:
+        pass
+    #se cambia a la ruta de los pdf
+    try:
+        os.chdir(wordToPdf)
+    except:
+        print("ruta para pdf no encontradaa")
+        sys.exit()
+
+    #itera cada word encontrado y convierte a pdf
+    for word_i in wordList:
+        print(word_i)
+        if es_docx(word_i):
+            filename, f_ext = os.path.splitext(word_i)#recupera nombre del archivo
+            docx2pdf.convert(os.path.join(escritorio, f"Android a Word//{word_i}"), os.path.join(wordToPdf, f"{filename}.pdf"))#nombre del pdf
